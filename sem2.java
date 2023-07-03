31587c7ac63a8fed5348daa26a64ddf9fd1ea295
Alexander <9032231638a@gmail.com>
2023-07-03 22:32:27 +0300

    11

--- sem2.java
+++ sem2.java
@@ -53,56 +53,57 @@
 
 // Решение
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
+// import java.io.FileWriter;
+// import java.io.IOException;
+// import java.util.Scanner;
 
-public class sem2 {
-    static String convertArrayToString(int []arr) {
-        StringBuilder stb = new StringBuilder();
-        for (int element : arr) {
-            stb.append(element).append(" ");
-        }
-        return stb.toString();
-    }
+// public class sem2 {
+//     static String convertArrayToString(int []arr) {
+//         StringBuilder stb = new StringBuilder();
+//         for (int element : arr) {
+//             stb.append(element).append(" ");
+//         }
+//         return stb.toString();
+//     }
 
-    static void sortByBubble(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = i; j < arr.length; j++) {
-                if (arr[i] > arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
-            }
-            writeToFile(convertArrayToString(arr));
-        }
-    }
+//     static void sortByBubble(int[] arr) {
+//         for (int i = 0; i < arr.length - 1; i++) {
+//             for (int j = i; j < arr.length; j++) {
+//                 if (arr[i] > arr[j]) {
+//                     int temp = arr[i];
+//                     arr[i] = arr[j];
+//                     arr[j] = temp;
+//                 }
+//             }
+//             writeToFile(convertArrayToString(arr));
+//         }
+//     }
 
-    static void writeToFile( String line) {
-        try (FileWriter fw = new FileWriter("fileForArray.txt", true)) {
-            fw.write(line);
-            fw.write("\n");
-            fw.flush();
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
+//     static void writeToFile( String line) {
+//         try (FileWriter fw = new FileWriter("result.txt", true)) {
+//             fw.write(line);
+//             fw.write("\n");
+//             fw.flush();
+//         } catch (IOException ex) {
+//             System.out.println(ex.getMessage());
+//         }
+//     }
 
-    static int[] convertFromStringToInt(String [] arr) {
-        int [] arrayResult = new int [arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            arrayResult[i] = Integer.parseInt(arr[i]);
-        }
-        return arrayResult;
-    }
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        System.out.printf("Введите числа через зпт: ");
-        String numbers = scn.nextLine();
-        numbers = numbers.replace(" ", "");
-        String [] arrayNumbers = numbers.split(",");
-        sortByBubble(convertFromStringToInt(arrayNumbers));
-        scn.close();
-    }
-}
+//     static int[] convertFromStringToInt(String [] arr) {
+//         int [] arrayResult = new int [arr.length];
+//         for (int i = 0; i < arr.length; i++) {
+//             arrayResult[i] = Integer.parseInt(arr[i]);
+//         }
+//         return arrayResult;
+//     }
+//     public static void main(String[] args) {
+//         Scanner scn = new Scanner(System.in);
+//         System.out.printf("Введите числа через зпт: ");
+//         String numbers = scn.nextLine();
+//         numbers = numbers.replace(" ", "");
+//         String [] arrayNumbers = numbers.split(",");
+//         sortByBubble(convertFromStringToInt(arrayNumbers));
+//         scn.close();
+//     }
+// }
+
