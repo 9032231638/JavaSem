addce30a3e76c7369df26d861e9167bdcd0ff221
Alexander <9032231638a@gmail.com>
2023-07-03 19:19:53 +0300

    11

--- sem1.java
+++ sem1.java
@@ -1,42 +1,56 @@
 // 1) Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
 // Решение :
 
-import java.util.Scanner;
+// import java.util.Scanner;
+// public class sem1 {
 
-public class sem1 {
-    static int getTriangleNumber(int n) {
-        int sum = 0;
-        for (int index = 0; index < n; index++) {
-            sum += index + 1;
-        }
-        return sum;
-    }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Введите число: ");
+//         int n = sc.nextInt();
+//         int sum = 0;
+//         int f = 1;
+//         for (int i = 1; i<=n; i++){
+//              sum+=i;            
+//              f*=i;
+//             }
+//         System.out.println(sum);
+//         System.out.println(f);
+//         sc.close();
 
-    static int getFactorial(int n) {
-        int result = 1;
-        for (int index = 0; index < n; index++) {
-            result *= index + 1;
-        }
-        return result;
-    }
+//     }
+// }
 
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int n = 0;
-        System.out.printf("Введите натуральное число: ");
-        if (!scn.hasNextInt()) {
-            System.out.println("Неверное число !");
-        } else {
-            n = scn.nextInt();
-            if (n < 1)
-                System.out.println("Введите положительное число !");
-            else {
-                System.out.printf("Ссума из %d = %s ", n, getTriangleNumber(n));
-                System.out.println();
-                System.out.printf("Произведение из %d = %s ", n, getFactorial(n));
-            }
-        }
-        scn.close();
-    }
-}
+// 2) Вывести все простые числа от 1 до 1000
+// Решение
 
+// import java.util.Scanner;
+// import java.util.ArrayList;
+// import java.util.List;
+
+// public class sem1 {
+
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.print("Введите положительное число: ");
+//         int input = scanner.nextInt();
+//         scanner.close();
+//         List<Integer> primes = new ArrayList<>();
+
+//         for (int i = 2; i <= input; i++) {
+//             boolean isPrimeNumber = true;
+
+//             for (int j = 2; j < i; j++) {
+//                 if (i % j == 0) {
+//                     isPrimeNumber = false;
+//                     break;
+//                 }
+//             }
+
+//             if (isPrimeNumber) {
+//                 primes.add(i);
+//             }
+//         }
+//         System.out.println("Простые числа: " + primes);
+//     }
+// }
